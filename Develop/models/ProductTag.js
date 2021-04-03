acdf0df038fcc6197ee908bc48031063586cbb99
@kcruit-salzberg <kassandra.cruit-salzberg@alsoenergy.com>
2021-04-02 21:43:43 -0600

    initial set up of db

---
+++ Develop/models/ProductTag.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
